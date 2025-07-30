de7df414c50cbf3dafdb58f4bcdc3d43ff865c44
loubnama <157687343+loubnama@users.noreply.github.com>
2025-07-30 04:13:53 +0300

    Update main.py

--- Smart_search/main.py
+++ Smart_search/main.py
@@ -85,5 +85,4 @@ async def search_by_image(file: UploadFile = File(...)):
     except Exception as e:
         return JSONResponse(content={"error": str(e)}, status_code=500)
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=10000)
+
